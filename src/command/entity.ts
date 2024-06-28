08ecb7b4469c3ead10b3c5b1dc73c8822a7e6376
Wesley F. Young <willamwei@outlook.com>
2024-06-28 23:56:39 +0800

    docs: add docs for `addHandler`

--- src/command/entity.ts
+++ src/command/entity.ts
@@ -10,6 +10,78 @@ export class Command {
     callback: (player: Player, args: any) => void;
   }[] = [];
 
+  /**
+   * Add a pattern and a corresponding handler.
+   *
+   * An example of using pattern:
+   * ```typescript
+   * // ... construct an API instance
+   *
+   * const cmd = new Command();
+   * cmd.addHandler(
+   *   [
+   *     'push',
+   *     [ 'up', 'down' ],
+   *     { type: 'integer', displayName: 'blocks' },
+   *     { type: 'boolean', displayName: 'break-original' }
+   *   ] as const,
+   *   (player, [, destination,        blocks,      breakOriginal]) => {
+   *     //        ^ 'up' | 'down'     ^ number     ^ boolean
+   *     //        IDE will infer correct types
+   *
+   *     // Your game logic here
+   *   }
+   * );
+   * api.registerCommand('test', cmd);
+   * ```
+   * In this example, because of the strange but strong type system of TypeScript,
+   * your IDE can infer the type of each parameter.
+   * Also notice that the [destructuring assignment](https://www.typescriptlang.org/docs/handbook/variable-declarations.html#destructuring)
+   * use here skipped the first argument, which is always `push`.
+   * You can also skip other fixed arguments.
+   *
+   * Some inputs that match this pattern could be:
+   * ```
+   * .test push up 114 true
+   * .test push down 514 false
+   * ```
+   *
+   * While such input
+   * ```
+   * .test push up xxx true
+   * ```
+   * won't match and will throw such error:
+   * ```
+   * May match pattern: push [up|down] [blocks: integer] [break-original: boolean]
+   *   - Error when parsing blocks: not a valid integer
+   * ```
+   * and such input
+   * ```
+   * .test xxx
+   * ```
+   * that do not match the length(`1` vs `4`) or action(`xxx` vs `push`) of any pattern(s)
+   * will throw such error:
+   * ```
+   * No pattern matched input. Possible:
+   *   - push [up|down] [blocks: integer] [break-original: boolean]
+   * ```
+   *
+   * @param pattern The pattern to add.
+   * Use with `as const` just like the example above to enable type inferring.
+   * A pattern is an array of three types of components:
+   * - A plain string (e.g. `'action'`):
+   * Only when the argument at this position is equal to the specified string, the parser will continue.
+   * - A string array (e.g. `[ 'up', 'down', 'left', 'right' ]`):
+   * Only when the argument at this position is one of the provided strings, the parser will continue.
+   * - An `Argument` (`{ type: 'string' | 'float' | 'integer' | 'boolean', displayName: string }`):
+   * Only when the argument at this position satisfies the specified type, the parser will continue.
+   *
+   * @param callback The callback function that handles parsed arguments.
+   * The provided function should accept two arguments:
+   * - `player`: the player that calls the command.
+   * - `args`: an array (tuple) that contains parsed arguments.
+   * The IDE will infer correct type for every input argument.
+   */
   addHandler<T extends Pattern>(
     pattern: T,
     callback: (player: Player, args: ParsePattern<T>) => void
